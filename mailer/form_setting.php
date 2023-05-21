03b6bf2f229e0aaa2c4a3e9a60608b7d3aa528be
sohrab.niroo <sohrab.niroo@gmail.com>
2023-05-21 10:28:36 +0330

    start

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'Sohrab Niroo - Your new Product Designer'"; //letter subject
+	$to = 'Sohrab.Niroo@gmail.com'; // Enter Your E-mail
+	$from = 'hire@sohrabniroo.com'; // Admin e-mail
+	$fromName = 'Sohrab Niroo'; // Your company name
+	$charset = 'UTF-8';
+?>
